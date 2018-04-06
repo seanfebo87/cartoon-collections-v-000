19ac60494007a61a7ae798a5814bbba6ec0a2e6a
Sean Febo <seanfebo@techie.com>
2018-04-06 21:23:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,13 +11,13 @@ def summon_captain_planet(array)
   end
 end
 
-def long_planeteer_calls(arr)
-    arr.any? do |call|
+def long_planeteer_calls(array)
+    array.any? do |call|
       call.length > 4
     end
 end
 
 def find_the_cheese(array)
-  
+  a
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
