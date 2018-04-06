dc3a4d399394bd4b74cc865a0133dc62a57dee24
Sean Febo <seanfebo@techie.com>
2018-04-06 21:11:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(array)
+  array.each_with_index do |name, i|
+    puts "#{i + 1}. #{name}"
+  end
+end
+  
+
+def summon_captain_planet(array)
+  array.map do |name|
+    name.capitalize + "!"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+   arr.map do |call|
+     binding.pry
+    if call.length > 4
+      return true
+    else
+      return false 
+    end
+  end
 end
 
 def find_the_cheese# code an argument here
